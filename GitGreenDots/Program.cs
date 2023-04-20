19f7e8def1721ec0b5a85e7a5728000563171f71
Blake Connally <blake@modsi.io>
2023-04-20 15:55:32 -0500

    All changes

--- GitGreenDots/Program.cs
+++ GitGreenDots/Program.cs
@@ -1,26 +1,53 @@
 ﻿using System.Diagnostics;
 
-Process cmd = new Process();
-cmd.StartInfo.FileName = "cmd.exe";
-cmd.StartInfo.RedirectStandardInput = true;
-cmd.StartInfo.RedirectStandardOutput = true;
-cmd.StartInfo.CreateNoWindow = true;
-cmd.StartInfo.UseShellExecute = false;
-cmd.Start(); // Start the processs ss
+
 
 var currentDayOfYear = DateTime.Now.DayOfYear;
 
-for (int i = 0; i < 2; i++)
+for (int i = 0; i < currentDayOfYear; i++)
 {
+    Process cmd = new Process();
+    cmd.StartInfo.FileName = "cmd.exe";
+    cmd.StartInfo.RedirectStandardInput = true;
+    cmd.StartInfo.RedirectStandardOutput = true;
+    cmd.StartInfo.CreateNoWindow = false;
+    cmd.StartInfo.UseShellExecute = false;
+    cmd.Start(); // Start the processs ss
+    
+    Console.WriteLine("-----------------------------");
+    Console.WriteLine($"Processing Day {i}");
+    Console.WriteLine("-----------------------------");
+
+    if (i == 35)
+    {
+        Thread.Sleep(1);
+    }
+    
     var dateUsed = new DateTime(DateTime.Now.Year, 1, 1).AddDays(i).ToString("U");
     // Update a file with the date
     cmd.StandardInput.WriteLine($"echo {dateUsed} >> green.txt");
+    
+    // Add all
+    cmd.StandardInput.WriteLine($"git add .");
 
     // Commit the file
-    cmd.StandardInput.WriteLine($"git commit --date=\"{currentDayOfYear - i} day ago\" -m \"{dateUsed}\" ");
+    try
+    {
+        cmd.StandardInput.WriteLine($"git commit --date=\"{currentDayOfYear - i} day ago\" -m \"{i}\" ");
+    }
+    catch (Exception e)
+    {
+        throw e;
+    }
+
+    // wait for one second to make sure the commit is done
+    cmd.StandardInput.Flush();
+    cmd.StandardInput.Close();
+    cmd.WaitForExit();
+    
+    System.Threading.Thread.Sleep(100);
+    
 }
 
-cmd.StandardInput.Flush();
-cmd.StandardInput.Close();
-cmd.WaitForExit();
-Console.WriteLine(cmd.StandardOutput.ReadToEnd());
+
+
